d883391e94257c74a97e7d751afb98c08d06d28c
coxs4 <coxs4@WITPF0TKD54.wit.private>
2018-11-12 13:55:48 -0500

    deleted this file and put main into questions (will be treated as server)

--- TriviaGame/src/Questions.java
+++ TriviaGame/src/Questions.java
@@ -1,34 +1,29 @@
 import java.util.Scanner;
 
-public class Questions {
-	public final int testLen = 5;
-	private String questions[] = new String[testLen];
-	private char answers[]= new char[testLen];
+public class Questions { //this is the questions class beginning
+	public final static int testLen = 5;
+	private static String questions[] = new String[testLen];
+	private static char answers[]= new char[testLen];
 	private int points = 0;
 
 	public Questions() {
-		System.out.println("Select the subject you would like to test: "+"\r\n"+"[1] Network Programming"+"\r\n"+"[2] Something Else");
-		Scanner input = new Scanner(System.in);
-		System.out.print("Your Choice: ");
-		int choice = input.nextInt();
 		
-		setQuestions(choice);
-		
+		//feed the questions, wait for an answer, and determine whether they answered correctly or not.
 		for(int q = 0; q<questions.length;q++) {
 			String ques = getQuestion(q);
 			System.out.println(ques);
 			
 			System.out.print("Answer: ");
-			char userAns = input.next().charAt(0);
+			char userAns = 'a'/*the button they press*/; //this is where the user makes their choice
 			char ans = getAnswer(q);
 			
 			if(userAns == ans) {
-				System.out.println("Correct!");
+				System.out.println("Correct!");	//calculate the correct answers
 				points++;
 			}
 			else System.out.println("Not quite..");
 		}
-		System.out.println("Test Complete. You scored "+points+"/5");
+		System.out.println("Test Complete. You scored "+points+"/5"); //return their score
 	}
 	
 	public char getAnswer(int num) {
@@ -39,15 +34,48 @@ public class Questions {
 		return questions[num];
 	}
 	
-	private void setQuestions(int test) {
+	public static void setQuestions(int test) {
 		if(test == 1) {
-			questions[1] = "This is the question"+"\r\n"+
-							"[A] this is an option"+"\r\n"+
-							"[B] this is another"+"\r\n"+
-							"[C] another"+"\r\n"+
-							"[D] one more";
-			questions[2] = "True or False: this is a question";
+			questions[0] = "Where were the 2012 Summer Olympics Held?"+"\r\n"+
+							"[A] Berlin"+"\r\n"+
+							"[B] Madrid"+"\r\n"+
+							"[C] London"+"\r\n"+ //correct
+							"[D] Moscow";
+			questions[1] = "True or False: Seinfeld is the current longest-running TV show.";
+			questions[2] = "Which of the following is NOT in Boston?"+"\r\n"+
+							"[A] Fenway Park"+"\r\n"+
+							"[B] Gillette Stadium"+"\r\n"+ //correct 
+							"[C] TD Garden"+"\r\n"+
+							"[D] None of the above";
+			questions[3] = "True or False: ";
+			questions[4] = "Which U.S. President was from the Boston area?"+"\r\n"+
+							"[A] Abraham Lincoln"+"\r\n"+
+							"[B] John F. Kennedy"+"\r\n"+ //correct
+							"[C] Jimmy Carter"+"\r\n"+
+							"[D] Bill Clinton";
 		
 		}
+	}
+	
+	public static void setAnswers(int num) {
+		if(num == 1) {
+			answers[0] = 2;
+			answers[1] = 1;
+			answers[3] = 1;
+			answers[4] = 1;
+		}	
+	}
+	
+	public static void main(String[] args) {
+		//prompt user to select a test type and then read their selection
+		System.out.println("Select the subject you would like to test: "+"\r\n"+"[1] General Trivia"+"\r\n"+"[2] Something Else");
+		Scanner input = new Scanner(System.in);
+		System.out.print("Your Choice: ");
+		int choice = input.nextInt();
+		
+		//set the test questions based on the user decision
+		setQuestions(choice);
+		setAnswers(choice);
+			
 	}
 }
