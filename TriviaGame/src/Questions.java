fe55fd7be9de14ea886c3f707c5bcd6a8c21749e
coxs4 <coxs4@WITPF0TKD54.wit.private>
2018-11-26 18:43:08 -0500

    newest questions from meeting

--- TriviaGame/src/Questions.java
+++ TriviaGame/src/Questions.java
@@ -1,43 +1,40 @@
 import java.io.BufferedReader;
-import java.io.DataInputStream;
+import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Questions { //this is the questions class beginning
 	public final static int testLen = 5;
 	private static String questions[] = new String[testLen];
 	private static int answers[]= new int[testLen];
-	private static String options[] = new String[4];
-	private static int points = 0;
+	
+
 
 	@SuppressWarnings("resource")
 	public static void main(String as[]) throws IOException
     {
-		ServerSocket serverSocket = new ServerSocket(2114);
-		//wait and accept a connection
+		ServerSocket serverSocket = new ServerSocket(1119);
 		Socket mySocket = serverSocket.accept(); 
+		
+		DataOutputStream dout = new DataOutputStream(mySocket.getOutputStream());
+		BufferedWriter write = new BufferedWriter(new OutputStreamWriter(dout));
 
 		setQuestions(1);
+		setAnswers(1);
 		
-		//feed the questions, wait for an answer, and determine whether they answered correctly or not.
-	    //DataInputStream din= new DataInputStream(mySocket.getInputStream());
-		DataOutputStream dout = new DataOutputStream(mySocket.getOutputStream());
-		//int receiveMessage;
+		
+		InputStream input = mySocket.getInputStream();
+		BufferedReader read  =  new BufferedReader(new InputStreamReader(input));
 		
 		for(int q = 0; q<questions.length;q++) {
 			dout.writeUTF(getQuestion(q));
 			dout.flush();
-			break;
 		}
-		//return score when the test concludes.
-	}
-	
-	public static int getAnswer(int num) {
-		return answers[num];
 	}
 	
 	public static String getQuestion(int num) {
@@ -46,11 +43,24 @@ public class Questions { //this is the questions class beginning
 	
 	public static void setQuestions(int test) {
 		if(test == 1) {
-			questions[0] ="Where were the 2012 Summer Olympics Held? 	A. Boston 	B. Hong Kong 	C. London 	D. Berlin \n";
-			questions[1] ="Which of the following is NOT in Boston? \n A. Fenway Park \n B. Gillete Stadium \n C. TD Garden \n D. None of the above. \n";
-			questions[2] ="Which U.S. President was from the Boston area? \n A. Jimmy Carter \n B. John F. Kennedy \n C. Samuel Adams \n D. Both B and C \n";
-			questions[3] ="A string";
-			questions[4] ="A string";
+			questions[0] ="3Where were the 2012 Summer Olympics Held? 	#A. Boston 	#B. Hong Kong 	#C. London 	#D. Berlin $";
+			questions[1] ="1Which of the following is NOT in Boston?		#A. Fenway   #B. Gillete Stadium 		#C. TD Garden 	#D. None of the above $";
+			questions[2] ="3Which President was from the Boston area?  	#A. Jimmy Carter 	#B. John F. Kennedy 		#C. Samuel Adams 	#D. Both B and C $";
+			questions[3] ="2In what year was Wentworth founded?			#A. 1900		#B. 1902		#C. 1904		#D.1906 $";
+			questions[4] ="1Which album sold the most copies in America? #A. Thriller - Michael Jackson #B. Greatest Hits - Eagles #C. DAMN. - Kendrick Lamar #D. In Pieces - Garth Brooks";
 		}
+	}
+	public static void setAnswers(int test) {
+		if(test == 1) {
+			answers[0] = 2;
+			answers[1] = 1;
+			answers[2] = 3;
+			answers[3] = 2;
+			answers[4] = 1;
+		}
+	}
+	
+	public static int getAnswer(int num) {
+		return answers[num];
 	}
 }
