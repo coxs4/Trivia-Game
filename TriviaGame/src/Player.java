a9fd3587f223c69ec547792b10687b4c12fc4f3a
coxs4 <coxs4@WITPF0TKD54.wit.private>
2018-11-26 10:17:39 -0500

    showing question and options with a,b,c,d buttons.

--- TriviaGame/src/Player.java
+++ TriviaGame/src/Player.java
@@ -3,48 +3,170 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 
-public class Player
-{
-    Socket s;
-    DataInputStream din;
-    DataOutputStream dout;
-    public Player()
-    {
-         try
-         {
-             String serverName = "10.200.240.150";
-             s=new Socket(serverName,2115);
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Player extends Application{
+	
+	public int ansCho = 0;
+	public String butOne ="Button";// getAnswer(1);
+	public String butTwo = "Button";// getAnswer(2);
+	public String butThree ="Button"; //getAnswer(3);
+	public String butFour = "Button";//getAnswer(4);
+	public String question = null;
+	Socket s;
+	DataInputStream din;
+	DataOutputStream dout;
+	
+
+	
+    public Player() {
+         try {
+             String serverName = "10.200.147.126";
+             s=new Socket(serverName,2114);
              System.out.println(s);
              din= new DataInputStream(s.getInputStream());
              dout= new DataOutputStream(s.getOutputStream());
              ClientChat();
          }
-         catch(Exception e)
-         {
+         catch(Exception e){
              System.out.println(e);
          }
      }
-     public void ClientChat() throws IOException {
+     @SuppressWarnings("null")
+	public void ClientChat() throws IOException{
            BufferedReader read= new BufferedReader(new InputStreamReader(din));
-           String receiveMessage, sendMessage;
-           while((receiveMessage = read.readLine()) != null)
-           {
-        		   System.out.println(receiveMessage.substring(2));
-        	   
+          String receiveMessage;           
+           while((receiveMessage = read.readLine())!= null) {
+        		   question = receiveMessage.substring(2);
+        		   
+        		   butOne = "A";
+        		   butTwo = "B";
+        		   butThree = "C";
+        		   butFour = "D";
               /* here is where code will go that will allow
                * a user to click a button representing their
                * answer. Send message will be button click.
                */
-        	   
         	   //what do do once a choice is made
-           }
-           while(!read.equals("stop"));
+           }  
     }
-    public static void main(String as[])
-    {
-        new Player(); 
-    }
-}
+  
+	
+	public void start(Stage primaryStage) throws Exception {
+		/*/
+		 * This is the gui and the button should update when an answer is selected  
+		 * was working
+		 */
+		
+		primaryStage.setTitle("Trivia Game");
+		GridPane grid = new GridPane();
+		grid.setGridLinesVisible(false);
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(75);
+		grid.setVgap(20);
+		
+		Text scenetitle = new Text("Trivia!");
+		scenetitle.setFont(Font.font("Tahoma", FontWeight.SEMI_BOLD, 30));
+		grid.add(scenetitle, 0, 0, 2, 1);
+		
+		Label userName = new Label("Question:");
+		grid.add(userName, 0, 1);
+		
+		Text score = new Text("Score: ");
+		grid.add(score, 0, 2);
+		
+		Text disScore = new Text("0");
+		grid.add(disScore, 1, 2);
+		
+		Text ask = new Text(question);
+		ask.setFont(Font.font("Tahoma", FontWeight.SEMI_BOLD, 12));
+		ask.setWrappingWidth(150);
+		grid.add(ask, 1,1,1, 1);
+		
+		
+		Button queOne = new Button(butOne);
+		Button queTwo = new Button(butTwo);
+		Button queThree = new Button(butThree);
+		Button queFour = new Button(butFour);
 
+		queOne.setOnAction(e->{
+			   ansCho=1;
+	           ask.setText(question);
+	           queOne.setText(butOne);
+	           queTwo.setText(butTwo);
+	           queThree.setText(butThree);
+	           queFour.setText(butFour);
+
+		});	
+		
+		grid.add(queOne, 0, 3);
+		
+		
+		queTwo.setOnAction(e->{
+			   ansCho=2;
+			   ask.setText(question);
+	           queOne.setText(butOne);
+	           queTwo.setText(butTwo);
+	           queThree.setText(butThree);
+	           queFour.setText(butFour);
+		});		
+		grid.add(queTwo, 1, 3);
+		
+		queThree.setOnAction(e->{
+			   ansCho=3;
+			   ask.setText(question);
+	           queOne.setText(butOne);
+	           queTwo.setText(butTwo);
+	           queThree.setText(butThree);
+	           queFour.setText(butFour);
+		});			
+		grid.add(queThree, 2, 3);
+	
+		queFour.setOnAction(e->{
+			   ansCho=4;
+			   ask.setText(question);
+	           queOne.setText(butOne);
+	           queTwo.setText(butTwo);
+	           queThree.setText(butThree);
+	           queFour.setText(butFour);
+		});			
+		grid.add(queFour, 3, 3);
+		
+		
+		try {
+			if(ansCho!=0) {
+				ClientChat();
+			  }
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		
+		Scene scene = new Scene(grid);
+		primaryStage.setResizable(false);
+		primaryStage.setScene(scene);
+		primaryStage.sizeToScene();
+		primaryStage.show();
+		
+	}
+	
+	public static void main(String args[]) {
+        launch(args);
+		new Player(); 
+	}	
+}
--- TriviaGame/src/Questions.java
+++ TriviaGame/src/Questions.java
@@ -2,12 +2,10 @@ import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Questions { //this is the questions class beginning
 	public final static int testLen = 5;
@@ -24,45 +22,17 @@ public class Questions { //this is the questions class beginning
 		Socket mySocket = serverSocket.accept(); 
 
 		setQuestions(1);
-		setAnswers(1);
 		
 		//feed the questions, wait for an answer, and determine whether they answered correctly or not.
-		//OutputStream output = mySocket.getOutputStream();
-		//PrintWriter write = new PrintWriter(output,true);
-	    DataInputStream din;
-        din= new DataInputStream(mySocket.getInputStream());
-		BufferedReader read= new BufferedReader(new InputStreamReader(din));
+	    //DataInputStream din= new DataInputStream(mySocket.getInputStream());
+		DataOutputStream dout = new DataOutputStream(mySocket.getOutputStream());
+		//int receiveMessage;
 		
-		DataOutputStream dout;
-		dout = new DataOutputStream(mySocket.getOutputStream());
-
-		int receiveMessage;
 		for(int q = 0; q<questions.length;q++) {
-			setOptions(q);
-			String ques = getQuestion(q);
-				
-			dout.writeUTF(ques);
+			dout.writeUTF(getQuestion(q));
 			dout.flush();
-			for(int n = 0; n < options.length; n++) {
-				String option = options[n];
-				dout.writeUTF(option);
-				dout.flush();
-				//System.out.println(options[n]);
-			}
-			
-				
-			/*int ans = getAnswer(q);
-			if((receiveMessage = read.readLine())!= 0) {
-      		   System.out.println(receiveMessage);
-      	    }
-			if(receiveMessage == ans) {
-				System.out.println("Correct!");	//calculate the correct answers
-				points++;
-			}	
-			else System.out.println("Not quite..");*/
-						
-			}
-		
+			break;
+		}
 		//return score when the test concludes.
 	}
 	
@@ -76,46 +46,11 @@ public class Questions { //this is the questions class beginning
 	
 	public static void setQuestions(int test) {
 		if(test == 1) {
-			questions[0] ="Where were the 2012 Summer Olympics Held?"+"\r\n";
-			questions[1] ="Which of the following is NOT in Boston?"+"\r\n";
-			questions[2] ="Which U.S. President was from the Boston area?"+"\r\n";
+			questions[0] ="Where were the 2012 Summer Olympics Held? [A. Boston B. Hong Kong C. London D. Berlin] \n";
+			questions[1] ="Which of the following is NOT in Boston? \n A. Fenway Park \n B. Gillete Stadium \n C. TD Garden \n D. None of the above. \n";
+			questions[2] ="Which U.S. President was from the Boston area? \n A. Jimmy Carter \n B. John F. Kennedy \n C. Samuel Adams \n D. Both B and C \n";
 			questions[3] ="A string";
 			questions[4] ="A string";
 		}
 	}
-	public static void setOptions(int question) {
-		if(question == 0) {
-			options[0] = "Boston";
-			options[1] = "Hong Kong";
-			options[2] = "London";
-			options[3] = "Berlin";
-		}
-		else if(question == 1) {
-			options[0] = "Fenway Park";
-			options[1] = "Gillete Stadium";
-			options[2] = "TD Garden";
-			options[3] = "None of the above";
-		}
-		else if(question == 2) {
-			options[0] = "Jimmy Carter";
-			options[1] = "John F. Kennedy";
-			options[2] = "Samuel Adams";
-			options[3] = "Jimmy Carter";
-		}
-		else{
-			options[0] = "an option";
-			options[1] = "an option";
-			options[2] = "an option";
-			options[3] = "an option";
-		}
-	}
-
-	public static void setAnswers(int num) {
-		if(num == 1) {
-			answers[0] = 2;
-			answers[1] = 1;
-			answers[3] = 1;
-			answers[4] = 1;
-		}	
-	}	
 }
